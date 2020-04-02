364c0d0cccfc266619a78a64859f13a4029c6771
Brian Lara <brian.lara@outlook.com.ar>
2020-04-02 18:25:08 -0300

    Crear Proyecto

---
+++ Clase3/Clase3/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Clase3
+//
+//  Created by Brian Lara on 02/04/2020.
+//  Copyright © 2020 Brian Lara. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
